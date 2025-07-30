e2a33d0afc94a620844e6336423c982d9ffa1b0c
Leomard0615 <fleomar513@gmail.com>
2025-07-30 12:10:03 -0400

    Add files via upload

---
+++ function calcularArea(ancho,largo) {.js
@@ -0,0 +1,19 @@
+function calcularArea(ancho,largo) {
+    const area = ancho * area;
+    return area;
+}
+
+const  calcuarPerimetro = function(ancho, largo) {
+    const ancho = 2* ( ancho + largo );
+    return ancho; 
+}
+
+const anchorectanulo= 4
+const largorectanglo= 5
+
+const anchoresultado= calcularArea ( anchorectangulo, largorectangulo );
+
+console.log(`El área de un rectángulo de ${anchoRectangulo}x${altoRectangulo} es ${areaResultante}.`);
+
+const perimetroResultante = calcularPerimetro(anchoRectangulo, altoRectangulo);
+console.log(`El perímetro del mismo rectángulo es ${perimetroResultante}.`);
