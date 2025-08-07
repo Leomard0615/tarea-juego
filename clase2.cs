4954b16f7e017b5126e605a53990fba942930a44
Leomard0615 <fleomar513@gmail.com>
2025-08-07 09:48:31 -0400

    Add files via upload

---
+++ clase2.cs
@@ -0,0 +1,29 @@
+
+using System;
+using System.Collections.Generic; // Necesario para usar Listas (List<T>)
+
+namespace FundamentosDeCSharp
+{
+
+     class ExplicacionGeneral
+     {
+
+        static void Main(string[] args)
+        {
+
+        Console.WriteLine("--- Explorando los Fundamentos de C# ---");
+
+            ExplicarVariables();
+            ExplicarArrays();
+            ExplicarListas();
+            ExplicarTuplas();
+            ExplicarFunciones();
+
+            Console.WriteLine("\n--- Fin de la Demostración ---");
+
+        }
+     }
+}
+
+        
+        
---
+++ clase3.cs
@@ -0,0 +1,107 @@
+
+
+ static void ExplicarListas();
+        {
+            Console.WriteLine("\n\n--- 3. Listas (List<T>) ---");
+
+            List<string> compañeros_de_clase = new List<string>();
+
+            Console.WriteLine("compañeros de clase");
+
+            compañeros_de_clase.Add("leomar ferrer, 16"):
+            compañeros_de_clase.Add("Maria Galleta, 22");
+            compañeros_de_clase.Add("Julio Cesar, 21");
+            compañeros_de_clase.Add("isaac Bracho");
+            
+            Console.WriteLine("Ahora la lista tiene " + compañeros_de_clase.Count + " clase.");
+
+            string [] nombre = new int[16];
+
+            nombre [16] Leomar ferrer
+            nombre [15]= Samuel Carabajal 
+            nombre [21]= Manria Galleta
+            nombre [17]= Nestor 
+            nombre [14]= Isaac
+            nombre [24]= Victor 
+
+
+        }
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
